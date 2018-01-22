25871e58d2c1e8396058e3dd41690fa2a91bcacd
aylliote <elliot.hofman@sondeos.com.ar>
2018-01-22 16:26:37 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,20 +8,26 @@ meta_features = ['source_reliable', 'journalist_likes_truth', 'editor_likes_trut
             'job_prone_truth', 'subject_prone_truth', 'state_prone_truth']
 
 ## parameters of Beta distributions for the bayesian smoothing
-BAYESIAN_PARAMETERS = {ALPHA_SOURCE : 2.761123
-                        BETA_SOURCE : 3.628518
+BAYESIAN_PARAMETERS = {"ALPHA_SOURCE" : 2.761123,
+                        "BETA_SOURCE" : 3.628518,
 
-                        ALPHA_JOURNALIST : 5.584136
-                        BETA_JOURNALIST : 8.167342
+                        "ALPHA_JOURNALIST" : 5.584136,
+                        "BETA_JOURNALIST" : 8.167342,
 
-                        ALPHA_EDITOR : 7.795503
-                        BETA_EDITOR : 10.641396
+                        "ALPHA_EDITOR" : 7.795503,
+                        "BETA_EDITOR" : 10.641396,
 
-                        ALPHA_JOB : 2.482408
-                        BETA_JOB : 3.815495
+                        "ALPHA_JOB" : 2.482408,
+                        "BETA_JOB" : 3.815495,
 
-                        ALPHA_SUBJECT : 15.24234
-                        BETA_SUBJECT : 19.17984
+                        "ALPHA_SUBJECT" : 15.24234,
+                        "BETA_SUBJECT" : 19.17984,
 
-                        ALPHA_STATE : 11.81635
-                        BETA_STATE : 15.61433}
+                        "ALPHA_STATE" : 11.81635,
+                        "BETA_STATE" : 15.61433}
+
+## usefull categorical sources
+
+usefull_sources = ['Barack Obama', 'Bill Clinton', 'Sherrod Brown', 'Rob Portman',
+                  'Tim Kaine', 'Dennis Kucinich', 'Bill Nelson', 'Chain email',
+                  'Bloggers', 'Democratic Congressional Campaign Committee']
